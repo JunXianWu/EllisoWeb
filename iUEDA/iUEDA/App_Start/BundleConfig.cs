38913a02a3905a8c852934f7b6e05531dc9cdaf7
Dennis Wu <jun.xian1006@gmail.com>
2018-08-27 01:35:49 +0800

    Add iUEDA

---
+++ iUEDA/iUEDA/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace iUEDA
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                      "~/Scripts/popper.min.js",
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/plugins.js",
+                      "~/Scripts/active.js"
+                      ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/style.css",
+                      "~/Content/responsive.css"));
+        }
+    }
+}
